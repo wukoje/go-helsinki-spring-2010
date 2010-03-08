7409a554212e13dbb745333851032ae9ba4996c7
Mikko Nieminen <mikko.nieminen@gmail.com>
2010-03-08 21:11:17 +0200

    Fizzbuzz
    R=kaib
    http://codereview.appspot.com/283041

--- mikko.nieminen/fizzbuzz.go
+++ mikko.nieminen/fizzbuzz.go
@@ -5,5 +5,21 @@ import (
 )
 
 func main() {
-	fmt.Printf("fizzbuzz.go\n")
+	for i := 1; i<=100; i++ {
+		x := 0
+		if  i%3 == 0 {
+			fmt.Printf("Fizz")
+			x++
+		}
+		if  i%5 == 0 {
+			fmt.Printf("Buzz")
+			x++
+		}
+		if x==0 {
+			fmt.Printf("%d ", i)
+		}
+
+		fmt.Printf("\n")
+	}
+
 }
