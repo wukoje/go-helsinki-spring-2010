a2a1186f903bd466d8b6ab8e6e5229bdf1152b4f
Martti Rannanj?rvi <martti.rannanjarvi@iki.fi>
2010-03-12 06:14:27 +0200

    Added slice_reversal.go
    R=kaib
    http://codereview.appspot.com/261042

---
+++ mrannanj/slice_reversal.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func reverse(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+func main() {
+	s := []int{0, 1, 2, 3, 4}
+	fmt.Println(s)
+	reverse(s)
+	fmt.Println(s)
+	reverse(s)
+	fmt.Println(s)
+}
