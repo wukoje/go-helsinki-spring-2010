0b687cc8857db459533d587c6578462d43629c7f
Janne Nikula <jni@iki.fi>
2010-03-12 08:10:47 +0200

    Add fizzbuzz.go
    R=kaib
    http://codereview.appspot.com/408043

---
+++ jni/fizzbuzz.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 100; i++ {
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			fmt.Printf("FizzBuzz\n")
+
+		case i%3 == 0:
+			fmt.Printf("Fizz\n")
+
+		case i%5 == 0:
+			fmt.Printf("Buzz\n")
+
+		default:
+			fmt.Printf("%d\n", i)
+		}
+	}
+}
