53d3aa3e74c434474c9bc6de9db645ee01281b2b
Aleksi Nurmi <aleksi.nurmi@cs.helsinki.fi>
2010-03-15 20:05:21 +0200

    implemented fizzbuzz
    R=kaib
    http://codereview.appspot.com/501041

---
+++ aleksi.nurmi/fizzbuzz.go
@@ -0,0 +1,17 @@
+
+package main
+
+func main() {
+	for i := 1; i <= 100; i++ {
+		switch {
+		case i % 3 == 0 && i % 5 == 0:
+			println("FizzBuzz")
+		case i % 3 == 0:
+			println("Fizz")
+		case i % 5 == 0:
+			println("Buzz")
+		default:
+			println(i)
+		}
+	}
+}
